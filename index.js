8bfad547c5d245af63325342ec1e650974f806ac
Pnina Breslav <pcrbus123@gmail.com>
2018-11-04 05:51:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function  destructivelyUpdateObjectWithKeyAndValue(object, key,value){
   object[key]=value;
   return object
 }
-function deleteFromObjectByKey destructivelyDeleteFromObjectByKey(object, key){
+function destructivelyDeleteFromObjectByKey(object, key){
   var object = {key};
   var newObj =Object.assign({}, object)
   return newObj;
